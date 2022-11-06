a1b91790bbe3503a012bd9dcbf7714a637d35ee4
Richard Melkonian <r.v.melkonian@gmail.com>
2022-11-06 00:14:18 +0000

    added more tests and pitchMake methods

--- main/maestro/pitch_maker.go
+++ main/maestro/pitch_maker.go
@@ -1,5 +1,7 @@
 package maestro
 
+import "fmt"
+
 type PitchMaker interface {
 	transposeUp(pitch, interval int) int
 	transposeDown(pitch, interval int) int
@@ -7,9 +9,26 @@ type PitchMaker interface {
 	transposeSetDown(pitchSet []int, interval int) []int
 	findInterval(pitch1, pitch2 int) int
 	findIntervals(pitchSet []int) []int
-	invertSet(pitchSet []int) []int
+	invertInterval(interval int) int
+	invertIntervals(intervals []int) []int
+	invertPitchSet(pitchSet []int) []int
 	retrogradeSet(pitchSet []int) []int
 	retrogradeInvertSet(pitchSet []int) []int
+}
+
+var inversionMap = map[int]int{
+	0:  0,
+	1:  11,
+	2:  10,
+	3:  9,
+	4:  8,
+	5:  7,
+	6:  6,
+	7:  5,
+	8:  4,
+	9:  3,
+	10: 2,
+	11: 1,
 }
 
 type pitchMake struct {
@@ -21,16 +40,26 @@ func NewPitchMaker() PitchMaker {
 }
 
 func (p *pitchMake) transposeUp(pitch, interval int) int {
+	fmt.Printf("transposing pitch %d up by interval %d \n", pitch, interval)
+	if pitch == 0 {
+		pitch = 12
+	}
 	if pitch+interval >= 12 {
 		return pitch + interval - 12
 	}
+	fmt.Printf("transposed pitch %d \n", pitch+interval)
 	return pitch + interval
 }
 
 func (p *pitchMake) transposeDown(pitch, interval int) int {
+	fmt.Printf("transposing pitch %d down by interval %d \n", pitch, interval)
+	if pitch == 0 {
+		pitch = 12
+	}
 	if pitch-interval < 0 {
 		return pitch - interval + 12
 	}
+	fmt.Printf("transposed pitch %d \n", pitch-interval)
 	return pitch - interval
 }
 
@@ -72,27 +101,75 @@ func (p *pitchMake) findInterval(pitch1, pitch2 int) int {
 	return result
 }
 
+func (p *pitchMake) invertInterval(interval int) int {
+	var i = interval
+
+	if i > 12 {
+		i = i - 12
+	}
+	return inversionMap[i]
+}
+
+func (p *pitchMake) invertIntervals(intervals []int) []int {
+	invertedIntervals := make([]int, len(intervals))
+
+	for i, interval := range intervals {
+		invertedIntervals[i] = p.invertInterval(interval)
+	}
+
+	return invertedIntervals
+}
+
+func (p *pitchMake) invertPitchSet(pitchSet []int) []int {
+	for _, pitch := range pitchSet {
+		fmt.Printf("pitch args%d \n", pitch)
+	}
+	invertedSet := make([]int, len(pitchSet))
+	invertedSet[0] = pitchSet[0]
+
+	for i, pitch := range pitchSet {
+		if i == 0 {
+			continue
+		}
+
+		interval := p.findInterval(pitchSet[i-1], pitch)
+		invertedInterval := p.invertInterval(interval)
+		if pitchSet[i-1] > pitch {
+			fmt.Printf("pitch-1 %d is greater than pitch %d \n", pitchSet[i-1], pitch)
+			invertedSet[i] = p.transposeUp(invertedSet[i-1], invertedInterval)
+		} else {
+			fmt.Printf("pitch-1 %d is less than pitch %d \n", pitchSet[i-1], pitch)
+			invertedSet[i] = p.transposeDown(invertedSet[i-1], interval)
+		}
+		fmt.Printf("invertedSet: %v \n", invertedSet)
+	}
+
+	return invertedSet
+}
+
+func getCircularNextPitch(pitchSet []int, i int) int {
+	var next int
+	if i == len(pitchSet)-1 {
+		next = pitchSet[0]
+		return makeZeroTheGreatest(next)
+	}
+	next = pitchSet[i+1]
+	return makeZeroTheGreatest(next)
+}
+
+func makeZeroTheGreatest(pitch int) int {
+	if pitch == 0 {
+		pitch = 12
+	}
+	return pitch
+}
+
 func (p *pitchMake) retrogradeSet(pitchSet []int) []int {
 	retrogradedSet := make([]int, len(pitchSet))
 	for i, _ := range pitchSet {
 		retrogradedSet[i] = pitchSet[len(pitchSet)-1-i]
 	}
-
 	return retrogradedSet
 }
 
-func (p *pitchMake) retrogradeInvertSet(pitchSet []int) []int {
-	retrogradedInvertedSet := make([]int, len(pitchSet))
-	for i, _ := range pitchSet {
-		retrogradedInvertedSet[i] = 12 - pitchSet[len(pitchSet)-1-i]
-
-		if retrogradedInvertedSet[i] == 12 {
-			retrogradedInvertedSet[i] = 0
-		}
-		if retrogradedInvertedSet[i] > 12 {
-			retrogradedInvertedSet[i] = retrogradedInvertedSet[i] - 12
-		}
-	}
-
-	return retrogradedInvertedSet
-}
+// work on matrix generation next
--- main/maestro/pitch_maker_test.go
+++ main/maestro/pitch_maker_test.go
@@ -1,6 +1,7 @@
 package maestro
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/rvmelkonian/maestro/main/shared"
@@ -87,7 +88,7 @@ func TestPitchMaker(t *testing.T) {
 		}
 	})
 
-	t.Run("findInterval should be correct for single dyad", func(t *testing.T) {
+	t.Run("findInterval should be correct for each dyad", func(t *testing.T) {
 		args := [][]int{
 			{0, 1},
 			{0, 7},
@@ -138,26 +139,75 @@ func TestPitchMaker(t *testing.T) {
 		}
 	})
 
-	// t.Run("chromaticInvertSet set should be correct", func(t *testing.T) {
-	// 	args := [][]int{
-	// 		{0, 2, 4, 5, 7, 9, 11},
-	// 		{0, 7, 2, 6, 1, 4, 11},
-	// 		{2, 8, 9, 3, 4, 10, 1},
-	// 	}
+	t.Run("invertInterval should correctly invert an single interval", func(t *testing.T) {
+		args := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+		want := []int{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
 
-	// 	want := [][]int{
-	// 		{0, 10, 8, 7, 5, 3, 1},
-	// 		{0, 5, 10, 6, 11, 8, 1},
-	// 		{2, 8, 7, 11, 10, 4, 1},
-	// 	}
+		for i := 0; i < len(args); i++ {
+			got := pitchMaker.invertInterval(args[i])
+			if got != want[i] {
+				t.Errorf("got %d want %d", got, want[i])
+			}
+		}
+	})
 
-	// 	for i, arg := range args {
-	// 		got := pitchMaker.invertSet(arg)
-	// 		fmt.Printf("arg %v \n got %v \n want %v \n", arg, got, want[i])
+	t.Run("invertIntervals should correctly invert a slice of intervals", func(t *testing.T) {
+		got := pitchMaker.invertIntervals([]int{
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+		})
+		want := []int{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
 
-	// 		if !shared.SliceEqual(got, want[i]) {
-	// 			t.Errorf("got %v want %v", got, want[i])
-	// 		}
-	// 	}
-	// })
+		if !shared.SliceEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("invertPitchSet should correctly invert a series of pitch sets", func(t *testing.T) {
+		args := [][]int{
+			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+			{0, 11, 10, 4, 2, 9, 3, 6, 8, 7, 1, 5},
+			{1, 6, 3},
+			{4, 2, 7, 5},
+			{6, 1, 8, 3, 10, 11},
+		}
+		want := [][]int{
+			{0, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			{0, 1, 2, 8, 10, 3, 9, 6, 4, 5, 11, 7},
+			{1, 8, 11},
+			{4, 6, 1, 3},
+			{6, 11, 4, 9, 2, 1},
+		}
+
+		for i := 0; i < len(args); i++ {
+			fmt.Printf("args: %v \n", args[i])
+			got := pitchMaker.invertPitchSet(args[i])
+			if !shared.SliceEqual(got, want[i]) {
+				t.Errorf("got %v want %v", got, want[i])
+			}
+		}
+	})
+
+	t.Run("retrogradeSet should correctly retrograte a series of pitch sets", func(t *testing.T) {
+		args := [][]int{
+			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+			{0, 11, 10, 4, 2, 9, 3, 6, 8, 7, 1, 5},
+			{1, 6, 3},
+			{4, 2, 7, 5},
+			{6, 1, 8, 3, 10, 11},
+		}
+		want := [][]int{
+			{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+			{5, 1, 7, 8, 6, 3, 9, 2, 4, 10, 11, 0},
+			{3, 6, 1},
+			{5, 7, 2, 4},
+			{11, 10, 3, 8, 1, 6},
+		}
+
+		for i := 0; i < len(args); i++ {
+			got := pitchMaker.retrogradeSet(args[i])
+			if !shared.SliceEqual(got, want[i]) {
+				t.Errorf("got %v want %v", got, want[i])
+			}
+		}
+	})
 }
